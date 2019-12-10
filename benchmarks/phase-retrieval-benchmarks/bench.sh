fb77ee14a22e03a6f17b8558f713eb6735cd0a75
Anthony Dowling <dowlinah@clarkson.edu>
2019-12-10 06:19:30 -0500

    fixing bug in bench.sh for phase retrieval

--- benchmarks/phase-retrieval-benchmarks/bench.sh
+++ benchmarks/phase-retrieval-benchmarks/bench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ( time ./RRR data/data100E 800 .95 .5 1000 5 results100E ) |& tee ./time.txt
-valgrind --tool=callgrind ./rrr data/data100e 800 .95 .5 1000 5 results100e 
-valgrind --tool=massif --pages-as-heap=yes --massif-out-file=massif.out ./rrr data/data100e 800 .95 .5 1000 5 results100e 
+valgrind --tool=callgrind ./RRR data/data100E 800 .95 .5 1000 5 results100E 
+valgrind --tool=massif --pages-as-heap=yes --massif-out-file=massif.out ./RRR data/data100E 800 .95 .5 1000 5 results100E 
 grep mem_heap_B massif.out | sed -e 's/mem_heap_B=\(.*\)/\1/' | sort -g | tail -n 1 | tee ./mem.txt
 
 
