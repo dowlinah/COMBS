48eecf9b00fff88c67382ff326928fb10c548b7b
Anthony Dowling <dowlinah@clarkson.edu>
2019-12-10 07:25:12 -0500

    adding changes that apparently were missed

--- benchmarks/hpcg/bench.sh
+++ benchmarks/hpcg/bench.sh
@@ -7,7 +7,7 @@ cd build/bin
 
 (time ./xhpcg) |& tee ./time.txt
 valgrind --tool=callgrind ./xhpcg
-valgrind --tool=massif --pages-as-heap=yes --massif-out-file=massif.out $1
+valgrind --tool=massif --pages-as-heap=yes --massif-out-file=massif.out ./xhpcg
 grep mem_heap_B massif.out | sed -e 's/mem_heap_B=\(.*\)/\1/' | sort -g | tail -n 1 | tee ./mem.txt
 
 cd ../..
